f8eaf896565c7897c026f4747a9e30509a652614
unknown <mwm0022@126.com>
2023-03-01 21:56:35 +0800

    new deno

---
+++ server.ts
@@ -0,0 +1,8 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+const HTML = await Deno.readFile("./index.html");
+serve((_req) => new Response(HTML, {
+  headers: {
+    "content-type": "text/html",
+  },
+}), { port: 3000 });
